2949b1364e7292d41e7bd02de5ab012350228118
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-03-10 10:10:38 +0100

    dekoratory wrzucone do jednego pliku

--- konto.py
+++ konto.py
@@ -1,37 +1,5 @@
 from sys import argv
-from manager import Manager, FileManager
-
-manager = Manager()
-file_manager = FileManager()
-
-
-@manager.assign("saldo", 2)
-def balance_update(manager, amount, comment):
-    if manager.balance + int(amount) >= 0:
-        manager.balance += int(amount)
-
-
-@manager.assign("zakup", 3)
-def buy(manager, product_name, price, amount):
-    if manager.balance - int(price)*int(amount) >= 0:
-        manager.balance -= int(price)*int(amount)
-        if product_name not in manager.stock:
-            manager.stock[product_name] = int(amount)
-        else:
-            manager.stock[product_name] += int(amount)
-    else:
-        print("Brak wystarczającej ilość gotówki.")
-
-
-@manager.assign("sprzedaz", 3)
-def sell(manager, product_name, price, amount):
-    if product_name not in manager.stock:
-        print("Brak produktu w magazynie!")
-    elif manager.stock[product_name] >= int(amount):
-        manager.stock[product_name] -= int(amount)
-        manager.balance += int(price)*int(amount)
-    else:
-        print("Brak wystarczającej ilości")
+from lib.accountant import manager, file_manager
 
 
 file_manager.open_file()
--- magazyn.py
+++ magazyn.py
@@ -1,40 +1,7 @@
 from sys import argv
-from lib.manager import Manager, FileManager
+from lib.accountant import manager, file_manager
 
-manager = Manager()
-file_manager = FileManager()
 products_to_view = argv[2:]
-
-@manager.assign("saldo", 2)
-def balance_update(manager, amount, comment):
-    if manager.balance + int(amount) >= 0:
-        manager.balance += int(amount)
-
-
-@manager.assign("zakup", 3)
-def buy(manager, product_name, price, amount):
-    if manager.balance - int(price)*int(amount) >= 0:
-        manager.balance -= int(price)*int(amount)
-        if product_name not in manager.stock:
-            manager.stock[product_name] = int(amount)
-        else:
-            manager.stock[product_name] += int(amount)
-    else:
-        print("Brak wystarczającej ilość gotówki.")
-        manager.input_error = True
-
-
-@manager.assign("sprzedaz", 3)
-def sell(manager, product_name, price, amount):
-    if product_name not in manager.stock:
-        print("Brak produktu w magazynie!")
-    elif manager.stock[product_name] >= int(amount):
-        manager.stock[product_name] -= int(amount)
-        manager.balance += int(price)*int(amount)
-    else:
-        print("Brak wystrczającej ilości produktu w magazynie.")
-        manager.input_error = True
-
 
 file_manager.open_file()
 file_manager.file_process(manager)
